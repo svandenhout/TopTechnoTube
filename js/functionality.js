73ee61c5aba287dab899bb814f759fca82381f3b
Steven Van den Hout <stevenvandenhout@gmail.com>
2013-01-17 15:02:48 +0100

    idem

--- js/functionality.js
+++ js/functionality.js
@@ -4,12 +4,12 @@ function popupVideo() {
     if(clicked === false){
         clicked = true;
         $("#frame").animate({
-            top: '-=600'
+            top: '-=660'
         });
     }else {
         clicked = false;
         $("#frame").animate({
-            top: '+=600'
+            top: '+=660'
         });
     }
 }
